9664d03f2657ea3696db79690375a746c93a2d9c
jialechern <jialechern@gmail.com>
2026-03-11 16:21:46 +0800

    feat(scripts/swayidle.sh)
    去除休眠功能.

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# 十五分钟自动锁屏、半小时熄屏、一小时休眠
+# 十五分钟自动锁屏、半小时熄屏、永不休眠
 swayidle -w \
     timeout 900     '~/.config/niri/scripts/swaylock.sh' \
     timeout 1800    'niri msg action power-off-monitors' \
-    resume          'niri msg action power-on-monitors' \
-    timeout 3600    'systemctl suspend'
+    resume          'niri msg action power-on-monitors'
 
